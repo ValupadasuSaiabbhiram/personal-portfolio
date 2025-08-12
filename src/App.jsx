3bfaa500dce062b26b2f0675b105fb333126740c
sai abbhiram <valupadasusaiabbhiram@gmail.com>
2025-08-12 15:49:22 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Home } from "./pages/Home";
+import { Toaster } from "@/components/ui/toaster"
+import { NotFound } from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Toaster/>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="*" element={<NotFound/>}/>
+
+
+    </Routes>
+    
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App

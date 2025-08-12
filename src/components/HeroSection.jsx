f2cf5386eee7733e46f80bbee601ffff816c2aad
Sai abbhiram Valupadasu <71780446+ValupadasuSaiabbhiram@users.noreply.github.com>
2025-08-12 20:36:13 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,7 +13,7 @@ export const HeroSection = () => {
                 </h1>
 
                 <p className= "text-lg md:text-xl text-muted foreground max-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-                    I create stellar web experiences with modren technologies.
+                    I create stellar web experiences with modern technologies.
                     Specializing in front-end development, I build interfaces that are 
                     both beautiful and functional.
                 </p>
